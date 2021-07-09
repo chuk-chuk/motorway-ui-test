7b8bfdc0821ed075340ec351bc7b1d2f90cd27b6
chuk-chuk <yulia.v.litvin@gmail.com>
2021-07-09 16:48:53 +0200

    Save url and port number in the env vars

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-'use strict';
-
 const app = require('express')();
 const images = require('./src/images.json');
 
-const randomInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const port = process.env.REACT_APP_PORT || 5000
+const url = process.env.REACT_APP_URL || "http://localhost:"
 
 app.get('/images', ({ query }, res) => {
   const i = (query.limit) ? images.slice(0, parseInt(query.limit)) : images;
-
-  setTimeout(() => {
-    return res.status(200).json(i);
-  }, randomInterval(500, 1500));
+  return res.status(200).json(i);
 });
 
 app.get(/^\/(car-images|avatars)\//, (req, res) => {
-  // If no extension, just serve the jpg file
   res.status(200).sendFile(`public/${req.url}.jpg`, { root: __dirname });
 });
 
-app.listen(5000, () => {
-  process.stdout.write('Server is available on http://localhost:5000/\n');
+app.listen(port, () => {
+  process.stdout.write(`Server is available on ${url}${port}/\n`);
 });
