d894514566474b79f3419f384924cd4ce000bd8e
chuk-chuk <yulia.v.litvin@gmail.com>
2021-07-12 01:08:10 +0200

    Add other fields

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,21 +4,72 @@ import Input from "../Input";
 import "./styles.css";
 
 const Form = () => {
-  const inputState = useInputState("", [
+  const inputStateName = useInputState("", [
+    notEmpty("Field is required"),
+  ])
+  const inputStateEmail = useInputState("", [
+    notEmpty("Field is required"),
+  ])
+  const inputStateDob = useInputState("", [
+    notEmpty("Field is required"),
+  ])
+  const inputStateColor = useInputState("", [
+    notEmpty("Field is required"),
+  ])
+  const inputStateSalary = useInputState("", [
     notEmpty("Field is required"),
   ])
 
   return (
     <>
     <Input
+      type="text"
+      fieldName="name"
+      labelText="Name"
+      value={inputStateName.value}
+      onChange={inputStateName.onChange}
+      onBlur={inputStateName.onBlur}
+      errorMessage={inputStateName.errorMessage}
+    />
+    <Input
       type="email"
       fieldName="email"
       labelText="Email"
-      value={inputState.value}
-      onChange={inputState.onChange}
-      onBlur={inputState.onBlur}
-      errorMessage={inputState.errorMessage}
+      value={inputStateEmail.value}
+      onChange={inputStateEmail.onChange}
+      onBlur={inputStateEmail.onBlur}
+      errorMessage={inputStateEmail.errorMessage}
     />
+    <Input
+      type="text"
+      fieldName="dob"
+      labelText="Date of birth"
+      value={inputStateDob.value}
+      onChange={inputStateDob.onChange}
+      onBlur={inputStateDob.onBlur}
+      errorMessage={inputStateDob.errorMessage}
+    />
+    <Input
+      type="color"
+      fieldName="color"
+      labelText="Favourite colour"
+      value={inputStateColor.value}
+      onChange={inputStateColor.onChange}
+    />
+     <Input
+      type="range"
+      min={0}
+      max={1000}
+      defaultValue={0} 
+      step={1}
+      fieldName="salary"
+      labelText="Salary"
+      value={inputStateSalary.value}
+      onChange={inputStateSalary.onChange}
+    />
+    <div className="button">
+      <button className="submit-button" onClick={() => {console.log("form submitted")}}>Submit</button>
+    </div>
     </>
   );
 }
