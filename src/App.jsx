1c146ca7412c7aa42be7fc8d0dbf560d27a390aa
chuk-chuk <yulia.v.litvin@gmail.com>
2021-07-11 11:44:27 +0200

    Add option to see the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Card from "./components/Card"
+import Form from "./components/Form"
 import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState();
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     fetch('images?limit=10')
@@ -20,13 +22,19 @@ const App = () => {
   return (
     <div className="app-container">
     <h2>Photo Gallery</h2>
-      {
+    <button className="form-button" onClick={() => setShowForm(true)}>Fill in form</button>
+      
+      {showForm ? <Form /> : (
+        <>
+        {
         images && images.map(img => (
           <div key={img.id} className="card-container" >
             <Card imageData={img}/>
           </div>
         ))
       }
+      </>
+      )}
     </div>
   );
 }
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,12 @@
+import React, {  useState } from "react";
+import "./styles.css";
+
+const Form = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <div>Form goes here</div>
+  );
+}
+
+export default Form;
